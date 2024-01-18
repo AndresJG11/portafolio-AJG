31dd017f1e32de0d79a05804900cc0cdfb8dfad1
AndrésJG <jg.andresjg@gmail.com>
2024-01-17 23:58:35 -0500

    feat(general): i8n, theme, header

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    defaultLocale: "es",
+    locales: ["en", "es",],
+    routing: {
+      prefixDefaultLocale: true
+    }
+  }
+});
---
+++ src/data/i18n/ui.ts
@@ -0,0 +1,22 @@
+export const languages = {
+    es: "Español",
+    en: "English",
+  };
+  
+export const defaultLang = "es";
+export const showDefaultLang = false;
+
+export const ui = {
+    es: {
+        "header.label_home": 'Inicio',
+        "header.label_research": 'Investigación',
+        "header.label_publications": 'Publicaciones',
+        "header.label_blogs": 'Blogs',
+    },
+    en: {
+        "header.label_home": 'Home',
+        "header.label_research": 'Research',
+        "header.label_publications": 'Publications',
+        "header.label_blogs": 'Blogs',
+    }
+}
