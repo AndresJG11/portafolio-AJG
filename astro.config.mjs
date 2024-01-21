455176f920bb64054271d75f61668d187b5a8ac2
AndrésJG <jg.andresjg@gmail.com>
2024-01-21 00:38:48 -0500

    feat(styles):

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config"
 
+import icon from "astro-icon";
+import alpine from '@astrojs/alpinejs';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon(), alpine()],
   i18n: {
     defaultLocale: "es",
     locales: ["en", "es",],
